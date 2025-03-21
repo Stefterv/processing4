0850ac929e8739dabcf50a989ea284f4faa432df
Stef Tervelde <stef@steftervelde.nl>
2025-03-21 16:24:06 +0100

    Share menu with pde files

--- app/src/processing/app/tools/Share.kt
+++ app/src/processing/app/tools/Share.kt
@@ -2,11 +2,17 @@ package processing.app.tools
 
 import androidx.compose.material.Text
 import processing.app.Sketch
+import processing.app.SketchCode
 import processing.app.ui.theme.LocalLocale
 import processing.app.ui.theme.PDEButton
 import processing.app.ui.theme.PDEWindow
-import java.net.URL
+import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.StringSelection
 import javax.swing.SwingUtilities
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
 
 class Share{
     companion object {
@@ -15,7 +21,8 @@ class Share{
             SwingUtilities.invokeLater {
                 PDEWindow("share.window.title"){
                     PDEButton(onClick = {
-
+                        val clipboard: Clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
+                        clipboard.setContents(StringSelection(sketch.toBase64URL()), null)
                     }){
                         val locale = LocalLocale.current
                         Text(locale["share.window.text"])
@@ -23,11 +30,30 @@ class Share{
                 }
             }
         }
-        fun convertSketchToBase64URL(sketch: Sketch): URL{
+        fun Sketch.toBase64URL(): String{
+            return convertSketchToBase64URL(this)
+        }
+        fun convertSketchToBase64URL(sketch: Sketch): String{
+            val main = sketch.code.first()
 
+            var url = "pde://sketch/base64/${main.toBase64()}"
 
-            return URL("https://processing.org");
+            val pdes = sketch.code
+                .drop(1)
+                .map{
+                    "${it.fileName}:${it.toBase64()}"
+                }
+                .joinToString(",")
+            if(pdes.isNotEmpty()){
+                url += "?pde=$pdes"
+            }
+            return url
         }
 
+        @OptIn(ExperimentalEncodingApi::class)
+        fun SketchCode.toBase64(): String{
+            return Base64.encode(program.toByteArray())
+
+        }
     }
 }
--- app/test/processing/app/tools/ShareTest.kt
+++ app/test/processing/app/tools/ShareTest.kt
@@ -30,6 +30,7 @@ class ShareTest{
                 callme();
             }
         """.trimIndent())
+
         val secondaryFile = tempDir.resolve("secondary.pde")
         secondaryFile.writeText("""
             void callme(){
@@ -39,6 +40,6 @@ class ShareTest{
 
         val sketch = Sketch(mainFile.toString(), mode)
         val url = Share.convertSketchToBase64URL(sketch)
-        assertEquals("https://processing.org", url.toString())
+        assertEquals("pde://sketch/base64/dm9pZCBzZXR1cCgpewogICAgc2l6ZSg0MDAsIDQwMCk7Cn0Kdm9pZCBkcmF3KCl7CiAgICBiYWNrZ3JvdW5kKDI1NSk7CiAgICBjYWxsbWUoKTsKfQ==?pde=secondary.pde:dm9pZCBjYWxsbWUoKXsKICAgIHByaW50bG4oIkhlbGxvIFdvcmxkIik7Cn0=", url)
     }
 }
