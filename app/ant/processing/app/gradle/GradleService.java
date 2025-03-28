3a14ff40a41979ff7ad25a144f34629c0405e3a0
Stef Tervelde <stef@steftervelde.nl>
2025-03-28 08:18:53 +0100

    Added Stubs

---
+++ app/ant/processing/app/gradle/GradleService.java
@@ -0,0 +1,13 @@
+package processing.app.gradle;
+
+import processing.app.ui.Editor;
+
+public class GradleService {
+    public GradleService(Editor editor) { }
+
+    public void prepare(){}
+    public void run() {}
+    public void export(){}
+    public void stop() {}
+    public void startService() {}
+}
