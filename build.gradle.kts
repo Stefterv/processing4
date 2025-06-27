bc617a5616ba2752f5934f09d2c18f4cb0f7575d
Stef Tervelde <stef@steftervelde.nl>
2025-06-27 08:50:16 +0200

    Initial idea

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,18 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
 }
 
+subprojects{
+    plugins.withType<JavaPlugin>() {
+        java {
+            toolchain {
+                languageVersion = JavaLanguageVersion.of(17)
+                vendor = JvmVendorSpec.ADOPTIUM
+            }
+        }
+    }
+}
+
+
 // Set the build directory to not /build to prevent accidental deletion through the clean action
 // Can be deleted after the migration to Gradle is complete
 layout.buildDirectory = file(".build")
