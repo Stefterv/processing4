49080cdfeeaff6e2090819cb0b9401af843585b9
Stef Tervelde <stef@steftervelde.nl>
2025-09-10 15:12:18 +0200

    PdeService POC complete

--- java/src/processing/mode/java/lsp/proxy/PdeService.kt
+++ java/src/processing/mode/java/lsp/proxy/PdeService.kt
@@ -7,6 +7,7 @@ import processing.app.Sketch
 import processing.app.contrib.ModeContribution
 import processing.mode.java.JavaMode
 import processing.mode.java.PreprocService
+import processing.mode.java.PreprocSketch
 import java.io.File
 import java.net.URI
 
@@ -34,23 +35,33 @@ class PdeService {
 
     val preprocServices = mutableMapOf<String, PreprocService>()
 
-    fun addSketchFile(uri: String){
+    fun addSketchFile(uri: String): PreprocSketch?{
         val uri = URI.create(uri)
 
-        val sketchFolder = File(uri).absolutePath
+        val sketch = File(uri)
+        if(sketch.extension != "pde"){
+            println("Not a PDE file: $uri")
+            return null
+        }
+        val sketchFolder = sketch.absolutePath
+
         // TODO: Switch to the main file or same as the parent folder name
         if (!preprocServices.containsKey(sketchFolder)){
             val sketch = Sketch(sketchFolder, mode!!)
             val preprocService = PreprocService(mode!!, sketch)
-
-            preprocService.notifySketchChanged()
-            preprocService.whenDone{
-                println("PreprocService initialized for sketch at $sketchFolder")
-                // After pre-processing is done, let the LSP server know about the files
-            }
-
             preprocServices[sketchFolder] = preprocService
         }
+        val preprocService = preprocServices[sketchFolder]!!
+
+        var result: PreprocSketch? = null
+
+        preprocService.notifySketchChanged()
+        preprocService.whenDoneBlocking{
+            result = it
+            println("PreprocService initialized for sketch at $sketchFolder")
+            // After pre-processing is done, let the LSP server know about the files
+        }
+        return result
     }
 
 }
