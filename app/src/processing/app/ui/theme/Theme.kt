413546064610dc4f5c7a4e8b29e213c1c8ba7905
Stef Tervelde <stef@steftervelde.nl>
2025-09-11 17:36:21 +0200

    Add test message triggers to Develop menu
    Introduces a new menu item in the Develop menu to trigger various message dialogs for testing purposes. Also adds TODO comments in Theme.kt for future UI improvements and references.

--- app/src/processing/app/ui/theme/Theme.kt
+++ app/src/processing/app/ui/theme/Theme.kt
@@ -12,6 +12,13 @@ import java.io.InputStream
 import java.util.Properties
 
 
+// TODO: Other monospace font
+// TODO: Look at flatlaf components
+// TODO: Look at Jetpack Compose default components
+// TODO: Message Boxes
+// TODO: Tabs
+// TODO: https://web.mit.edu/6.005/www/sp14/psets/ps4/java-6-tutorial/components.html
+
 class Theme(themeFile: String? = "") : Properties() {
     init {
         load(ClassLoader.getSystemResourceAsStream("theme.txt"))
