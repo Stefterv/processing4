490c3a539b53cb6b10379d0d825437186545c6b2
Stef Tervelde <stef@steftervelde.nl>
2025-10-21 14:45:35 +0200

    Refactor preferences UI and add General group
    Reworked Preferences UI to support dynamic preference groups and searching. Added new General and Other preference groups with controls, and updated language properties for advanced preferences. Introduced PDEPreference and PDEPreferenceGroup data classes for extensibility.

--- app/src/processing/app/ui/Preferences.kt
+++ app/src/processing/app/ui/Preferences.kt
@@ -6,83 +6,169 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.DrawerValue
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import processing.app.LocalPreferences
+import processing.app.ui.preferences.General
+import processing.app.ui.theme.LocalLocale
 import processing.app.ui.theme.PDETheme
+
+val LocalPreferenceGroups = compositionLocalOf<MutableMap<PDEPreferenceGroup, List<PDEPreference>>> {
+    error("No Preference Groups Set")
+}
 
 class Preferences {
     companion object{
+        val groups = mutableStateMapOf<PDEPreferenceGroup, List<PDEPreference>>()
+        fun register(preference: PDEPreference) {
+            val list = groups[preference.group]?.toMutableList() ?: mutableListOf()
+            list.add(preference)
+            groups[preference.group] = list
+        }
+        init{
+            General.register()
+        }
+
 
         @OptIn(ExperimentalMaterial3Api::class)
         @Composable
         fun preferences(){
-            Row {
-                NavigationRail(
-                    header = {
-                        Text("Settings", style = MaterialTheme.typography.titleLarge, modifier = Modifier.padding(top = 42.dp))
-
-                    },
-                    modifier = Modifier.defaultMinSize(minWidth = 200.dp)
-                ) {
-                    NavigationRailItem(
-                        selected = true,
-                        onClick = { /*TODO*/ },
-                        icon = {
-                            Icon(Icons.Default.Settings, contentDescription = null)
-                        },
-                        label = {
-                            Text("General")
-                        }
-                    )
-                    NavigationRailItem(
-                        selected = false,
-                        onClick = { /*TODO*/ },
-                        icon = {
-                            Icon(Icons.Default.Settings, contentDescription = null)
-                        },
-                        label = {
-                            Text("Interface & fonts")
-                        }
-                    )
+            var visible by remember { mutableStateOf(groups) }
+            val sortedGroups = remember {
+                val keys = visible.keys
+                keys.toSortedSet {
+                        a, b ->
+                    when {
+                        a.after == b -> 1
+                        b.after == a -> -1
+                        else -> a.name.compareTo(b.name)
+                    }
                 }
-                Box(modifier = Modifier.padding(top = 42.dp)){
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        SearchBar(inputField = {
-                            SearchBarDefaults.InputField(
-                                query = "",
-                                onQueryChange = { },
-                                onSearch = {
+            }
+            var selected by remember { mutableStateOf(sortedGroups.first()) }
+            CompositionLocalProvider(
+                LocalPreferenceGroups provides visible
+            ) {
+                Row {
+                    NavigationRail(
+                        header = {
+                            Text(
+                                "Settings",
+                                style = MaterialTheme.typography.titleLarge,
+                                modifier = Modifier.padding(top = 42.dp)
+                            )
 
+                        },
+                        modifier = Modifier.defaultMinSize(minWidth = 200.dp)
+                    ) {
+
+                        for (group in sortedGroups) {
+                            NavigationRailItem(
+                                selected = selected == group,
+                                enabled = visible.keys.contains(group),
+                                onClick = {
+                                    selected = group
+                                },
+                                icon = {
+                                    group.icon()
+                                },
+                                label = {
+                                    Text(group.name)
+                                }
+                            )
+                        }
+                    }
+                    Box(modifier = Modifier.padding(top = 42.dp)) {
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            var query by remember { mutableStateOf("") }
+                            val locale = LocalLocale.current
+                            LaunchedEffect(query){
+
+                                snapshotFlow { query }
+                                    .debounce(100)
+                                    .collect{
+                                        if(it.isBlank()){
+                                            visible = groups
+                                            return@collect
+                                        }
+                                        val filtered = mutableStateMapOf<PDEPreferenceGroup, List<PDEPreference>>()
+                                        for((group, preferences) in groups){
+                                            val matching = preferences.filter { preference ->
+                                                if(preference.key.contains(it, ignoreCase = true)){
+                                                    return@filter true
+                                                }
+                                                val description = locale[preference.descriptionKey]
+                                                description.contains(it, ignoreCase = true)
+                                            }
+                                            if(matching.isNotEmpty()){
+                                                filtered[group] = matching
+                                            }
+                                        }
+                                        visible = filtered
+                                    }
+
+                            }
+                            SearchBar(
+                                inputField = {
+                                    SearchBarDefaults.InputField(
+                                        query = query,
+                                        onQueryChange = {
+                                            query = it
+                                        },
+                                        onSearch = {
+
+                                        },
+                                        expanded = false,
+                                        onExpandedChange = {  },
+                                        placeholder = { Text("Search") }
+                                    )
                                 },
                                 expanded = false,
-                                onExpandedChange = { },
-                                placeholder = { Text("Search") }
-                            )
-                        }, expanded = false, onExpandedChange = {}, modifier = Modifier.align(Alignment.End).padding(16.dp)) {
+                                onExpandedChange = {},
+                                modifier = Modifier.align(Alignment.End).padding(16.dp)
+                            ) {
 
+                            }
+                            val prefs = LocalPreferences.current
+                            val preferences = visible[selected] ?: emptyList()
+                            LazyColumn {
+                                items(preferences){ preference ->
+                                    Text(
+                                        text = locale[preference.descriptionKey],
+                                        modifier = Modifier.padding(20.dp),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    preference.control(prefs[preference.key]) { newValue ->
+                                        prefs[preference.key] = newValue
+                                    }
+                                }
+                            }
                         }
-                        Text("Preferences go here")
                     }
                 }
             }
@@ -90,7 +176,6 @@ class Preferences {
 
         @JvmStatic
         fun main(args: Array<String>) {
-
             application {
                 Window(onCloseRequest = ::exitApplication){
                     remember{
@@ -110,9 +195,35 @@ class Preferences {
                         preferences()
                     }
                 }
-
             }
-
         }
     }
 }
+
+data class PDEPreference(
+    /**
+     * The key in the preferences file used to store this preference.
+     */
+    val key: String,
+    val descriptionKey: String,
+    /**
+     * The group this preference belongs to.
+     */
+    val group: PDEPreferenceGroup,
+    val control: @Composable (preference: String?, updatePreference: (newValue: String) -> Unit) -> Unit = { preference, updatePreference ->  },
+)
+
+data class PDEPreferenceGroup(
+    /**
+     * The name of this group.
+     */
+    val name: String,
+    /**
+     * The icon representing this group.
+     */
+    val icon: @Composable () -> Unit,
+    /**
+     * The group that comes before this one in the list.
+     */
+    val after: PDEPreferenceGroup? = null,
+)
